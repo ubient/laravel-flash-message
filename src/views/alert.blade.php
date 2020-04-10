19b36f83ddfd501d4ccca976616512968cf33a31
Claudio Dekker <claudiodekker@users.noreply.github.com>
2020-04-10 14:49:32 +0200

    Upgrade Tailwind colors

--- src/views/alert.blade.php
+++ src/views/alert.blade.php
@@ -1,10 +1,10 @@
 @if (session('flash_message') || session('status'))
     <div role="alert" class="{{ collect([
-        'info' => 'bg-blue-light',
-        'success' => 'bg-green-light',
-        'warning' => 'bg-orange',
-        'error' => 'bg-red-light',
-    ])->get(session('flash_message.level', 'success')) }} rounded shadow-inner text-white p-4">
+        'info' => 'bg-blue-400',
+        'success' => 'bg-green-400',
+        'warning' => 'bg-orange-500',
+        'error' => 'bg-red-400',
+    ])->get(session('flash_message.level', 'success')) }} rounded shadow-inner text-white-500 p-4">
         <p class="text-sm">{{ session('flash_message.message', session('status')) }}</p>
     </div>
     @php(session()->forget('flash_message'))
